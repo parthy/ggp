9e9b1981cbbadafd4908a6fea9c8e21dfa4eaa97
Markus <administrator@administrators-powerbook-g4-15.local>
2009-11-08 20:36:18 +0100

    Added rudimentary behavious when not having found a solution.

--- src/de/tudresden/inf/ggp/basicplayer/IDSStrategy.java
+++ src/de/tudresden/inf/ggp/basicplayer/IDSStrategy.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.eclipse.palamedes.gdl.core.model.IFluent;
 import org.eclipse.palamedes.gdl.core.model.IGameNode;
 import org.eclipse.palamedes.gdl.core.model.IGameState;
 import org.eclipse.palamedes.gdl.core.model.IMove;
@@ -16,10 +17,12 @@ public class IDSStrategy extends AbstractStrategy {
 	private List<IGameNode> queue = new ArrayList<IGameNode>();
 	private List<IGameNode> currentWay = new ArrayList<IGameNode>();
 	private Map<Integer, IGameState> visitedStates = new HashMap<Integer, IGameState>();
+	private Map<Integer, List<IFluent>> visitedStates_playing = new HashMap<Integer, List<IFluent>>();
 	private Map<Integer, IGameState> unusefulStates = new HashMap<Integer, IGameState>();
 	private boolean foundSolution = false;
 	private int currentDepthLimit;
 	private int nodesVisited;
+	private long endTime;
 	IGameNode solution;
 	IGameNode node;
 	IGameNode sol;
@@ -30,10 +33,10 @@ public class IDSStrategy extends AbstractStrategy {
 		// initiate solution search
 		game = initMatch.getGame();
 		queue.add(game.getTree().getRootNode());
+		endTime = System.currentTimeMillis() + initMatch.getStartTime()*1000 - 5000L;
 		IDS(1);
-		System.out.println("Solution found: "+solution);
-		fillCurrentWay();
-		System.out.println(currentWay);
+		visitedStates.clear();
+		if(foundSolution) fillCurrentWay();
 	}
 
     @Override
@@ -41,15 +44,27 @@ public class IDSStrategy extends AbstractStrategy {
     	/*
     	 * return the head element of currentWay and remove it from the list
     	 */
+    	System.out.println(currentNode);
+    	long endOfPlayTime = System.currentTimeMillis() + match.getPlayTime()*1000 - 500L;
     	if(!currentWay.isEmpty()) return currentWay.remove(0).getMoves()[0];
-		else // if no way is there, just do something.
+		else {// if no way is there, just do something.
+			visitedStates_playing.put(currentNode.getState().getFluents().hashCode(), null); // remember we were here
 			try {
-				return game.getRandomMove(currentNode)[0]; 
+				// try to return a move that will not lead us back somewhere we were already. if not possible, take random shot.
+				IMove[] movesToReturn;
+				IMove moveToReturn;
+				do {
+					movesToReturn = game.getRandomMove(currentNode);
+					moveToReturn = movesToReturn[0];
+				} while(System.currentTimeMillis() < endOfPlayTime && visitedStates_playing.containsKey(game.getNextNode(currentNode, movesToReturn).getState().getFluents().hashCode()));
+				 
+				return moveToReturn;
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			return currentNode.getMoves()[0]; // should not happen
+		}
     }
     
     void IDS(int depthLimit) {
@@ -58,7 +73,8 @@ public class IDSStrategy extends AbstractStrategy {
     	while(!foundSolution) {
 			System.err.println("currentDepth"+currentDepthLimit);
 			System.err.println("unusefulStates:"+unusefulStates.size());
-    		while(!queue.isEmpty()) {
+			System.err.println("Now: "+System.currentTimeMillis()+", endTime: "+endTime);
+    		while(!queue.isEmpty() && System.currentTimeMillis() < endTime) {
     			node = queue.remove(0);
     			
     			// try to regenerate state information of the node
@@ -99,7 +115,7 @@ public class IDSStrategy extends AbstractStrategy {
     				
     			}
     		}
-    		if(foundSolution) return;
+    		if(foundSolution || System.currentTimeMillis() >= endTime) return;
     		queue.clear();
     		visitedStates.clear();
     		queue.add(game.getTree().getRootNode());
